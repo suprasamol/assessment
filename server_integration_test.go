bbb405e04225b9dbac72d8accaec5e9f52841979
Suprasamol Tuppiree <suprasamol@gmail.com>
2023-01-04 22:36:10 +0700

    create test

---
+++ server_integration_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func uri(paths ...string) string {
+	host := "http://localhost" + os.Getenv("PORT")
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+func TestCreateExpense(t *testing.T) {
+	var e Expense
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath", 
+		"tags": ["food", "beverage"]
+	}`)
+	err := request(http.MethodPost, uri("expenses"), body).Decode(&e)
+	if err != nil {
+		t.Fatal("can't create expense: ", err)
+	}
+
+	var ex []Expense
+	res := request(http.MethodGet, uri("expenses"), nil)
+	err = res.Decode(&ex)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+	assert.Greater(t, len(ex), 0)
+
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
+}
