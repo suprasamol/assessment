6dafc636e2f20a71255ddfab74d9cb53a4a50d11
Suprasamol Tuppiree <suprasamol@gmail.com>
2023-01-05 21:54:29 +0700

    extract package

---
+++ expense/get.go
@@ -0,0 +1,53 @@
+package expense
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
+func GetExpensesHandler(c echo.Context) error {
+	stmt, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expense statement"})
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expenses"})
+	}
+
+	expenses := []Expense{}
+	for rows.Next() {
+		var e Expense
+		err = rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expenses"})
+		}
+		expenses = append(expenses, e)
+
+	}
+	return c.JSON(http.StatusOK, expenses)
+}
+
+func GetExpenseHandler(c echo.Context) error {
+	id := c.Param("id")
+	stmt, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses WHERE id = $1")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query expense statment:" + err.Error()})
+	}
+
+	row := stmt.QueryRow(id)
+	e := Expense{}
+	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+	switch err {
+	case sql.ErrNoRows:
+		return c.JSON(http.StatusNotFound, Err{Message: "expense not found"})
+	case nil:
+		return c.JSON(http.StatusOK, e)
+	default:
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expense:" + err.Error()})
+	}
+}
